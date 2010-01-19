cf3a06af14906c0ec23d7a110ea7ef288c054f3f
Gaurav Sharma <gaurav.qwe@gmail.com>
2010-01-19 16:33:06 +0530

    updated the movie name fetcher script

--- lib/myimdb/scraper/string_extensions.rb
+++ lib/myimdb/scraper/string_extensions.rb
@@ -23,6 +23,17 @@ module Myimdb
       def humanize
         gsub(/_/, " ").capitalize
       end
+      
+      def constantize
+        names = self.split('::')
+        names.shift if names.empty? || names.first.empty?
+      
+        constant = Object
+        names.each do |name|
+          constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+        end
+        constant
+      end
     end
   end
 end
